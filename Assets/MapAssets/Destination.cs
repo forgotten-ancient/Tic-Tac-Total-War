4a00cf5706c18ce856b6a7834be6c138f67ad75d
LeviathansRose <tsmattole@gmail.com>
2025-03-24 14:16:34 -0700

    Player Square Detection
    Essentially, this goes through all destinations tagged with "square". Then it checks to see if playerControlled is enabled. Then it checks for the closest distance squared (because performance). And finally it sets that unit's destination as the one closest to it. To see this example in action click player then space bar. This allows dynamic changes to the map, so it doesn't necessarily have to be limited to a 3x3, it doesn't even have to be a square.

---
+++ Assets/MapAssets/Destination.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+
+public class Destination : MonoBehaviour
+{
+    [SerializeField] public bool playerControlled;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            EnemyAI[] allEnemies = Object.FindObjectsByType<EnemyAI>(FindObjectsSortMode.None); // Finds all EnemyAI instances
+
+            foreach (EnemyAI enemy in allEnemies)
+            {
+                enemy.MoveEnemies(); // Call MoveEnemies() on each enemy
+            }
+        }
+
+    }
+}
---
+++ Assets/MapAssets/EnemyAI.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent agent;
+    public void MoveEnemies()
+    {
+        GameObject[] SquareObjects = GameObject.FindGameObjectsWithTag("Square"); // Get all objects with the given tag
+        Transform bestTarget = null;
+        float closestDistanceSqr = Mathf.Infinity;
+        Vector3 currentPosition = transform.position;
+
+        foreach (GameObject Square in SquareObjects)
+        {
+            Destination destination = Square.GetComponent<Destination>();
+            if (destination != null && destination.playerControlled)
+            {
+                Transform potentialTarget = Square.transform;
+                Vector3 directionToTarget = potentialTarget.position - currentPosition;
+                float dSqrToTarget = directionToTarget.sqrMagnitude;
+
+                if (dSqrToTarget < closestDistanceSqr)
+                {
+                    closestDistanceSqr = dSqrToTarget;
+                    bestTarget = potentialTarget;
+                }
+            }
+        }
+
+        if (bestTarget != null)
+        {
+            agent.SetDestination(bestTarget.position);
+        }
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
