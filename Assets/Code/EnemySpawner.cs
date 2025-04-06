b951de25aaccad01c6197aa8c10eb5221f23cfd8
Joel <Joelwaite20@gmail.com>
2025-04-05 19:26:16 -0700

    Finished!

--- Assets/Code/EnemySpawner.cs
+++ Assets/Code/EnemySpawner.cs
@@ -6,25 +6,28 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] private int MaximumEnemiesToStart = 8; // Maximum number of enemies to spawn
     [SerializeField] private int MinimunEnemiesToStart = 3; // Minimum number of enemies to spawn
     [SerializeField] private GameObject Enemy; // Prefab of the enemy to spawn
-    private GameObject[] SquareObjects;
+    [SerializeField] private CellManager[] Cells;
+    [SerializeField] private float SpawnTime = 3f; // Time interval between enemy spawns
+    [SerializeField] private float SpawnTimeMax = 6f; // Maximum spawn time
+    [SerializeField] private float SpawnTimeMin = 3f; // Minimum spawn time
+    [SerializeField] private float SpawnChance = 0.2f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        SquareObjects = GameObject.FindGameObjectsWithTag("Square"); // Get all objects with the given tag
-        foreach (GameObject square in SquareObjects) // Iterate through each square object
+        foreach (CellManager cell in Cells) // Iterate through each cell object
         {
             int randomAmount = Random.Range(MinimunEnemiesToStart, MaximumEnemiesToStart); // Randomly select the number of enemies to spawn
             for (int i = 0; i < randomAmount; i++)
             {
-                GameObject unit = Instantiate(Enemy, square.transform.position, Quaternion.identity); // Spawn the enemy at the square's position
+                GameObject unit = Instantiate(Enemy, cell.transform.position, Quaternion.identity); // Spawn the enemy at the cell's position
                 Vector3 move = new Vector3(
-                    square.transform.position.x+0.1f, 
-                    square.transform.position.y, 
-                    square.transform.position.z); // Create a vector to move the enemy a little bit
+                    cell.transform.position.x+0.1f, 
+                    cell.transform.position.y, 
+                    cell.transform.position.z); // Create a vector to move the enemy a little bit
                 NavMeshAgent unitAgent = unit.GetComponent<NavMeshAgent>(); // Get the NavMeshAgent component of the enemy
 
-                unitAgent.SetDestination(move); // Set the destination of the enemy to the square's transform
+                unitAgent.SetDestination(move); // Set the destination of the enemy to the cell's transform
             }
         }
     }
@@ -32,6 +35,28 @@ public class EnemySpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (SpawnTime > 0)
+        {
+            SpawnTime -= Time.deltaTime; // Decrease the spawn time by the time since the last frame
+        }
+        else
+        {
+            foreach (CellManager cell in Cells) // Iterate through each cell object
+            {
+                float randomValue = Random.Range(0f, 1f); // Generate a random value between 0 and 1
+                if (randomValue < SpawnChance && !cell.isPlayerOwned) // Check if the random value is less than the spawn chance
+                {
+                    GameObject unit = Instantiate(Enemy, cell.transform.position, Quaternion.identity); // Spawn the enemy at the cell's position
+                    Vector3 move = new Vector3(
+                        cell.transform.position.x + 0.1f,
+                        cell.transform.position.y,
+                        cell.transform.position.z); // Create a vector to move the enemy a little bit
+                    NavMeshAgent unitAgent = unit.GetComponent<NavMeshAgent>(); // Get the NavMeshAgent component of the enemy
+
+                    unitAgent.SetDestination(move); // Set the destination of the enemy to the cell's transform
+                }
+            }
+            SpawnTime = Random.Range(SpawnTimeMin, SpawnTimeMax); // Reset the spawn time to a random value between the minimum and maximum
+        }
     }
 }
