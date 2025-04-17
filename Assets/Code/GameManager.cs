f40cd7ba9a75c9eba92ccdbf2345ec9c5c0f37e0
LeviathansRose <tsmattole@gmail.com>
2025-04-17 11:23:03 -0700

    Fully Imported Shop branch

---
+++ Assets/Code/GameManager.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public int coins = 500;
+    public int currentSoldiers = 0;
+
+
+    private void Awake()
+    {
+        
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+
+
+            coins = PlayerPrefs.GetInt("Coins", 500);
+            currentSoldiers = PlayerPrefs.GetInt("Soldiers", 0);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public bool SpendCoins(int amount)
+    {
+        if (coins >= amount)
+        {
+            coins -= amount;
+            PlayerPrefs.SetInt("Coins", coins);
+            return true;
+        }
+        return false;
+    }
+
+    public void AddSoldier()
+    {
+        currentSoldiers++;
+        PlayerPrefs.SetInt("Soldiers", currentSoldiers);
+    }
+
+    public void SaveProgress()
+    {
+        PlayerPrefs.SetInt("Coins", coins);
+        PlayerPrefs.SetInt("Soldiers", currentSoldiers);
+    }
+
+    public void ResetProgress()
+    {
+        coins = 500;
+        currentSoldiers = 0;
+        PlayerPrefs.SetInt("Coins", coins);
+        PlayerPrefs.SetInt("Soldiers", currentSoldiers);
+    }
+}
