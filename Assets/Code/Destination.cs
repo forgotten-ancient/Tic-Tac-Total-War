fdcc0c622e832ec07d8cc2f52be1105a7fedf08c
LeviathansRose <tsmattole@gmail.com>
2025-04-17 14:07:23 -0700

    Adjusted Camera View & Fixed Index Exception in Destination Code

--- Assets/Code/Destination.cs
+++ Assets/Code/Destination.cs
@@ -6,7 +6,7 @@ public class Destination : MonoBehaviour
     [SerializeField] private float maxAttackTime = 3f; // Maximum time to wait before attacking
     [SerializeField] private float minAttackTime = 1f; // Minimum time to wait before attacking
     [SerializeField] private float attackTime = 1f; // starting attack time
-    
+
     [SerializeField] private int maxEnemiesToAttack = 5; // Maximum number of enemies to attack at once
     [SerializeField] private int minEnemiesToAttack = 1; // Minimum number of enemies to attack at once
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -23,11 +23,14 @@ public class Destination : MonoBehaviour
             EnemyAI[] allEnemies = Object.FindObjectsByType<EnemyAI>(FindObjectsSortMode.None); // Finds all EnemyAI instances
 
             int enemiesToAttack = Random.Range(minEnemiesToAttack, maxEnemiesToAttack + 1); // Randomly select number of enemies to attack
-            for (int i = 0; i < enemiesToAttack; i++)
+            if (allEnemies.Length != 0)
             {
-                int randomIndex = Random.Range(0, allEnemies.Length); // Select a random enemy from the array
-                EnemyAI selectedEnemy = allEnemies[randomIndex]; // Get the selected enemy
-                selectedEnemy.MoveEnemies(); // Call MoveEnemies() on the selected enemy
+                for (int i = 0; i < enemiesToAttack; i++)
+                {
+                    int randomIndex = Random.Range(0, allEnemies.Length); // Select a random enemy from the array
+                    EnemyAI selectedEnemy = allEnemies[randomIndex]; // Get the selected enemy
+                    selectedEnemy.MoveEnemies(); // Call MoveEnemies() on the selected enemy
+                }
             }
 
             attackTime = Random.Range(minAttackTime, maxAttackTime); // Reset attack time to a random value between min and max
